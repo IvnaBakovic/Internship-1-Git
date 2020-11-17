b3a5b32fcd0bb5c827e9b499189a7ffd4cb112b6
“IvnaBakovic” <ibakovic@pmfst.hr>
2020-11-17 19:06:12 +0100

    first hello message

---
+++ dumpGit/dumpGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dumpGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Duje, this is first commit!");
+        }
+    }
+}
