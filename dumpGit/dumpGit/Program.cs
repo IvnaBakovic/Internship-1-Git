ad81eeacc8a5440202f6c720e3252f7061dd0dcf
dujesaric <dsaric02@fesb.hr>
2020-11-18 18:08:20 +0100

    Second commit by Duje.

--- dumpGit/dumpGit/Program.cs
+++ dumpGit/dumpGit/Program.cs
@@ -7,6 +7,7 @@ namespace dumpGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Duje, this is first commit!");
+            Console.WriteLine("Hi Ivna, pleasure to work with you!!!");
         }
     }
 }
